2bc0c16516cfb6801f55aaa1652f503fec94ecec
Dor <dor.coh@gmail.com>
2019-01-21 21:52:34 +0200

    fixed generate

--- parser/generateCompFile.py
+++ parser/generateCompFile.py
@@ -7,6 +7,6 @@ comp_file = Data('resources/comp.unlabeled', comp=True)
 f = pickle_load('features.pickle')
 w = pickle_load('w.pickle')
 
-clf = Perceptron(features_tuple_pick=f, test_data=comp_file, comp=True)
+clf = Perceptron(features_tuple_pick=f, test_data=comp_file, comp=True, w=w)
 y_pred = clf.predict(comp_file)
 tag_comp(y_pred, comp_file, 'comp.labeled')
