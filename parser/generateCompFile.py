64cc6504cdcc6b9b1969a2306c3d6968c2cc8d8e
itaigat <itaigat.mail@gmail.com>
2019-01-21 21:19:14 +0200

    Generate labeled comp file

---
+++ parser/generateCompFile.py
@@ -0,0 +1,12 @@
+from parser.decoder import Data
+from parser.common import pickle_load, tag_comp
+from parser.classifier import Perceptron
+
+
+comp_file = Data('resources/comp.unlabeled', comp=True)
+f = pickle_load('features.pickle')
+w = pickle_load('w.pickle')
+
+clf = Perceptron(features_tuple_pick=f, test_data=comp_file, comp=True)
+y_pred = clf.predict(comp_file)
+tag_comp(y_pred, comp_file, 'comp.labeled')
